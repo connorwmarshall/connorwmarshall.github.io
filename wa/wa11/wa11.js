ee0c014ac0d5c8c12105ee197d730d4f63a95f7e
connorwmarshall <coma5672@colorado.edu>
2023-11-12 22:37:22 -0700

    changed imgs

--- wa/wa11/wa11.js
+++ wa/wa11/wa11.js
@@ -6,16 +6,16 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const imgArray = ['pic1.jpeg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const imgArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 
 /* Declaring the alternative text for each image file */
 
 var altDict = {
-    'pic1.JPEG': "Bodie sleeping",
-    'pic2.JPG': "Summer smiling",
+    'pic1.jpg': "Bodie sleeping",
+    'pic2.jpg': "Summer smiling",
     'pic3.jpg': "Summer and Bodie smiling",
-    'pic4.JPG': "Summer and Bodie lounging by the window",
+    'pic4.jpg': "Summer and Bodie lounging by the window",
     'pic5.jpg': "Puppy Bodie"
 };
 
